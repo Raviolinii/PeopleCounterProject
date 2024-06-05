29885f3b6d651ed75af9a4b639cadfe349786015
Krzysztof Garlacz <54943478+Raviolinii@users.noreply.github.com>
2024-06-05 23:14:16 +0200

    cleanup

--- PeopleCounter/RabbitInitialize.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import subprocess
-import time
-
-subprocess.run(["rabbitmq-plugins", "enable", "rabbitmq_mqtt"])
-time.sleep(10)
-subprocess.run(["rabbitmqadmin", "declare", "queue", "name=CounterQueue"])
-subprocess.run(["rabbitmqadmin", "declare", "binding", "source=amq.topic", "destination=CounterQueue", "routing_key=Counter"])
